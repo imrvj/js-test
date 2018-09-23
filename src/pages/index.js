7b88dc8db6890c0baa50c10a9056aab17311e4c1
RV <sng.rv07@gmail.com>
2018-09-23 21:22:57 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css';
+export default () => <div>
+    <div className="Header">
+    <h1>JS-TEST IN GATSBY.JS</h1>
+    
+    </div>
+  
+    <section className="bg-light">
+        <div className="container ">
+            <div className="row">
+                <div className="col-md-3"><a href="https://picsum.photos/200/300/"><img src="https://picsum.photos/200/300/" className="img-fluid img-thumbnail"></img></a><figcaption className="text">SUNSET</figcaption></div>
+                <div className="col-md-3"><a href="https://picsum.photos/200/300?image=0"><img src="https://picsum.photos/200/300?image=0"className="img-fluid img-thumbnail"></img></a><figcaption className="text">LAPTOP</figcaption></div>
+                <div className="col-md-3"><a href="https://picsum.photos/200/300/?gravity=east"><img src="https://picsum.photos/200/300/?gravity=east"className="img-fluid img-thumbnail"></img></a><figcaption className="text">CACTUS</figcaption></div>
+                <div className="col-md-3"><a href="https://picsum.photos/200/300/?random"><img src="https://picsum.photos/200/300/?random"className="img-fluid img-thumbnail"></img></a><figcaption className="text">RANDOM</figcaption></div>
+               
+                </div>
+
+    </div>
+    </section>
+    <div>
+        <p class="head">"CLICK ON IMAGE FOR PREVIEW"</p>
+    </div>
+
+    <div className="footer">
+    <p>&copy; 2018 Ranvijay Singh</p>
+
+    </div>
+    </div>
+
+
+                
+
+    
+    
+    
+    
+    
+    
+    
+    
+
